0554bdaf11d2db244d5e630fec2018d0cf2b04f3
Resul <garnnet_221@hotmail.com>
2022-01-18 01:21:38 +0300

    SynvVar Callbacks

--- Assets/Scripts/MyNetworkPlayer.cs
+++ Assets/Scripts/MyNetworkPlayer.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
+using TMPro;
 using UnityEngine;
 
 public class MyNetworkPlayer : NetworkBehaviour
 {
-    [SyncVar] [SerializeField] private string _playerDisplayName = "Missing Name";
-    [SyncVar] [SerializeField] private Color _playerColor = Color.white;
+    [SerializeField] private Renderer _playerRenderer;
+    [SerializeField] private TextMeshProUGUI _playerDisplayText;
+    
+    [SyncVar(hook = nameof(HandleUpdatePlayerDisplayText))] [SerializeField] private string _playerDisplayName = "Missing Name";
+    [SyncVar(hook = nameof(HandleUpdatePlayerColor))] [SerializeField] private Color _playerColor = Color.white;
 
     [Server]
     public void SetPlayerDisplayName(string newPlayerDisplayName)
@@ -18,5 +22,15 @@ public class MyNetworkPlayer : NetworkBehaviour
     public void SetPlayerColor(Color newColor)
     {
         _playerColor = newColor;
+    }
+
+    private void HandleUpdatePlayerColor(Color oldColor, Color newColor)
+    {
+        _playerRenderer.material.SetColor("_BaseColor", newColor);
+    }
+    
+    private void HandleUpdatePlayerDisplayText(string oldDisplayName, string newDisplayName)
+    {
+        _playerDisplayText.text = newDisplayName;
     }
 }
